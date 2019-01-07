4d1d0015de75a957a15d54984c4103f9c2d9b83c
Shuai Chen <wasd2144@hotmail.com>
2019-01-07 14:19:51 +0800

    fix babel will cover

--- index.js
+++ index.js
@@ -5,5 +5,7 @@ module.exports = function sourceLoader() {
   const content = source
     .replace(/\u2028/g, '\\u2028')
     .replace(/\u2029/g, '\\u2029');
-  return `module.exports = ${content}`;
+  return `module.exports = ${JSON.stringify(content)}`;
 };
+
+module.exports.seperable = true;
